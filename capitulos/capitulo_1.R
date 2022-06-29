3b768629c2982d53c9964291092d6777c606b883
armandios19 <fooseaov@gmail.com>
2022-06-29 11:55:16 -0500

    primer capitulo

--- capitulos/capitulo_1.R
+++ capitulos/capitulo_1.R
@@ -1,3 +1,12 @@
 # Instalación
 
-source("https://bioconductor.org/biocLite.R")
+if (!require("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+
+BiocManager::install(version = "3.15")
+
+
+# instalando y cargando paqueterias
+
+BiocManager::install("BSgenome")
+library(BSgenome) #las paqueterias se cargan igual
