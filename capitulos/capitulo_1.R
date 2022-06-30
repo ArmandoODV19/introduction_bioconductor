87e494ac5a10d8eabef6bf48f35113ed0d739cf2
armandios19 <fooseaov@gmail.com>
2022-06-29 20:09:27 -0500

    clase

--- capitulos/capitulo_1.R
+++ capitulos/capitulo_1.R
@@ -14,3 +14,23 @@ library(BSgenome) #las paqueterias se cargan igual
 # definir si un objeto es s4
 isS4()
 # o con str(), si es s4 empezará la descripción con el argumento Formal class
+
+
+# para crear una clase se utiliza la funcion setClass
+
+MyEpicProject <- setClass(#define class with upper camel case
+  "MyEpicProject",
+  # define slots
+  slots = c(ini = "Date",
+            end = "Date",
+            milestone = "character"),
+  # define inheritance
+  contains = "MyProject")
+
+# la funcion .S4methods(class = "project") genera un resumen de los accesorios
+
+.S4methods()
+
+# para obtener un resumen por objeto se utiliza la funcion show()
+show(MyDescriptor)
+
