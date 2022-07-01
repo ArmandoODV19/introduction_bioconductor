54b189477af411f4a625eb8390351dd9d7c8368d
armandios19 <fooseaov@gmail.com>
2022-07-01 12:41:42 -0500

    capitulo 2

--- capitulos/capitulo_2.R
+++ capitulos/capitulo_2.R
@@ -1,1 +1,8 @@
-##
+# la paqueteria Biostrings permite manipular secuencias biologicas
+
+BiocManager::install("Biostrings")
+library(Biostrings)
+
+DNA_BASES
+RNA_BASES
+AA_STANDARD #aa en una letra
