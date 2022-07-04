7a0cec5526615d96a1365204fd51d8811d242706
armandios19 <fooseaov@gmail.com>
2022-07-04 18:15:36 -0500

    clase

--- capitulos/capitulo_4.R
+++ capitulos/capitulo_4.R
@@ -1,1 +1,13 @@
-#
+# los formatos para guardar secuencias son fastq y fasta
+# los fastq incluyen formato de codificacion de calidad
+# la terminacion de formatos fastq es fastq y fq
+# la terminacion de formatos fasta es fa, fasta o seq
+
+# para leer archivos fasta se utiliza la funcion readfasta() de la paqueteria ShortRead
+# puede leer archivos compresos o libres
+
+BiocManager::install("ShortRead")
+
+library(ShortRead)
+
+fasample <- readFasta(dir = "data/zika.fa", pattern = "fasta")
