45e184906c7fb2491efa2b9affa90f77fe786f35
armandios19 <fooseaov@gmail.com>
2022-07-04 12:38:33 -0500

    clase 3

--- capitulos/capitulo_3.R
+++ capitulos/capitulo_3.R
@@ -19,3 +19,26 @@ myIRanges_end
 # se puede hacer un IRanges y especificar en qué numero empezar a partir de booleanos
 # en este ejemplo se eliminan  los primeros dos elemntos
 IRanges(c(FALSE, FALSE, TRUE, TRUE))
+
+# cargar genomic Rangers
+library(GenomicRanges)
+
+# la funcion GRanges() guarda intervalos genómicos por cromosoma
+# los argumentos minimos son el cromosoma, inicio y fin del intervalo
+# y se definen en formato caracter
+GRanges()
+GRanges("chr1:200-300")
+
+
+# la funcion as() permite transformar un data frame en un objeto GRangers
+
+seqnames <- c("chrX", "chrX", "chrX", "chrY", "chrY")
+start <- c(50,130,153,30,50)
+end <- c(120,140,154,40,55)
+strand <- c("+", "+", "+", "*", "-")
+GC <- c(0.25, 0.25, 0.25, 0.25, 0.25)
+
+mydf <- data.frame(seqnames, start, end, strand, GC)
+
+myGR <- as(mydf, "GRanges")
+myGR
